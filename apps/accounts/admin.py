76730f523c6fda9852ee3f4e9e6cb092efb787ef
LAN <konglanstudio@gmail.com>
2026-05-15 09:59:09 +0800

    feat: Admin 后台新增手机号字段显示

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("手机号", {"fields": ("phone", "phone_verified")}),
+    )
+    list_display = ("username", "email", "phone", "phone_verified", "is_staff")
+    search_fields = ("username", "email", "phone")
