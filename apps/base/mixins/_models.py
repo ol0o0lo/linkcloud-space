5d3552a80ce6590c00178613d0195c5fc9b2fb46
LAN <konglanstudio@gmail.com>
2026-06-01 23:26:12 +0800

    feat: 新增审计模型 Mixin

--- apps/base/mixins/_models.py
+++ apps/base/mixins/_models.py
@@ -9,6 +9,27 @@ class TimeStampModelMixin(models.Model):
         abstract = True
 
 
+class CreateUpdateTimeModelMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class AuditModelMixin(models.Model):
+    created_by = models.CharField(max_length=150, blank=True)
+    updated_by = models.CharField(max_length=150, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModelMixin(CreateUpdateTimeModelMixin, AuditModelMixin):
+    class Meta:
+        abstract = True
+
+
 class BaseListModelMixin(TimeStampModelMixin):
     is_active = models.BooleanField(default=True)
 
